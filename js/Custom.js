e9a96373cf5b13ebf6f380b6e3f7ea916aadfd82
WonderFunk <mark.guissine@gmail.com>
2015-04-30 13:24:21 -0400

    Fixed video scaling

--- js/Custom.js
+++ js/Custom.js
@@ -1,3 +1,7 @@
+//Enables WOW.js
+new WOW().init();
+
+
 // jQuery for page scrolling feature
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
@@ -9,3 +13,40 @@ $(function() {
     });
 });
 
+
+//Video resizing code
+ jQuery(document).ready(function(){
+            $('video#intro-video').on("loadedmetadata", scaleVideo);
+                function scaleVideo(){
+                    
+                    //Stores window dimensions as variables
+                    var windowWidth = $(window).width();
+                    var windowHeight = $(window).height();
+            
+                    //Stores video dimensions as variables
+                    var videoNativeWidth = $('video#intro-video')[0].videoWidth;
+                    var videoNativeHeight = $('video#intro-video')[0].videoHeight;
+                    
+                    //stores scale factors                   
+                    var heightScale = windowHeight / videoNativeHeight;
+                    var widthScale = windowWidth / videoNativeWidth;
+
+                    //Stores heighest scale factor                  
+                    if(widthScale > heightScale) {
+                        var scale = widthScale;
+                    }
+                    else {
+                        var scale = heightScale;
+                    }
+                    
+                    //Stores scaled video dimensions                
+                    var videoScaledWidth = videoNativeWidth * scale;
+                    var videoScaledHeight = videoNativeHeight * scale;
+                    
+                    //Apply scaled dimensions
+                    $('video#intro-video').height(videoScaledHeight);
+                    $('video#intro-video').width(videoScaledWidth);
+                }
+        })
+ 
+ 
