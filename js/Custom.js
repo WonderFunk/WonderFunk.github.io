e51820500f53fd4321590bf02d13c1367a8410ea
WonderFunk <mark.guissine@gmail.com>
2015-04-30 21:51:50 -0400

    Few major changes - See description
    - Added transparency to navbar when fixed and to ease into new css class
    on scroll
    -optimized video scaling to be more lightweight
    -started adding some fonts to section headers

--- js/Custom.js
+++ js/Custom.js
@@ -2,51 +2,14 @@
 new WOW().init();
 
 
-// jQuery for page scrolling feature
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    
+    var videoHeight = jQuery('video').height();
+    
+    if ($(".navbar").offset().top > parseInt(videoHeight - 100)) {
+        $(".navbar-fixed-top").addClass("nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("nav-collapse");
+    }
 });
-
-
-//Video resizing code
- jQuery(document).ready(function(){
-            $('video#intro-video').on("loadedmetadata", scaleVideo);
-                function scaleVideo(){
-                    
-                    //Stores window dimensions as variables
-                    var windowWidth = $(window).width();
-                    var windowHeight = $(window).height();
-            
-                    //Stores video dimensions as variables
-                    var videoNativeWidth = $('video#intro-video')[0].videoWidth;
-                    var videoNativeHeight = $('video#intro-video')[0].videoHeight;
-                    
-                    //stores scale factors                   
-                    var heightScale = windowHeight / videoNativeHeight;
-                    var widthScale = windowWidth / videoNativeWidth;
-
-                    //Stores heighest scale factor                  
-                    if(widthScale > heightScale) {
-                        var scale = widthScale;
-                    }
-                    else {
-                        var scale = heightScale;
-                    }
-                    
-                    //Stores scaled video dimensions                
-                    var videoScaledWidth = videoNativeWidth * scale;
-                    var videoScaledHeight = videoNativeHeight * scale;
-                    
-                    //Apply scaled dimensions
-                    $('video#intro-video').height(videoScaledHeight);
-                    $('video#intro-video').width(videoScaledWidth);
-                }
-        })
- 
- 
